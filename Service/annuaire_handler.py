d2bd7b5a98f09fb41ccc11046ce3e0ae57bab050
Clément Pickel <clement.pickel@epitech.eu>
2025-09-30 10:45:52 +0200

    change query to work hopefully

--- Service/annuaire_handler.py
+++ Service/annuaire_handler.py
@@ -17,20 +17,24 @@ class AnnuaireHandler:
         )
         self.cur = self.conn.cursor()
 
+    def tuple_to_str(self, t: tuple) -> str:
+        return "(" + ", ".join(str(item) for item in t) + ")"
+
     def get_id(self, email: str):
-        self.cur.execute("SELECT IDINDIVIDU FROM ADRESSEEMAIL WHERE LOWER(ADRMAIL) = LOWER(%s)", (email,))
+        self.cur.execute(f"SELECT IDINDIVIDU FROM ADRESSEEMAIL WHERE LOWER(ADRMAIL) = LOWER({email})")
         result = self.cur.fetchone()
         return result if result else None
 
     def get_id_sage(self, email: str):
-        self.cur.execute("SELECT IDENTIFIANTCONTACTPN FROM BENEVOLES_SAGE WHERE LOWER(EMAIL1) = LOWER(%s)", (email,))
+        self.cur.execute(f"SELECT IDENTIFIANTCONTACTPN FROM BENEVOLES_SAGE WHERE LOWER(EMAIL1) = LOWER({email})")
         result = self.cur.fetchone()
         return result if result else None
     
     def get_sage_info(self, person_ids: tuple[int]):
         if not person_ids:
             return None
-        self.cur.execute("SELECT * FROM BENEVOLES_SAGE WHERE IDENTIFIANTCONTACTPN = %s", (person_ids,))
+        query = f"SELECT * FROM BENEVOLES_SAGE WHERE IDENTIFIANTCONTACTPN IN {self.tuple_to_str(person_ids)}"
+        self.cur.execute(query)
         data = self.cur.fetchall()
         cols = [desc[0] for desc in self.cur.description]
         return pd.DataFrame(data, columns=cols)
@@ -39,7 +43,8 @@ class AnnuaireHandler:
     def get_pn_email(self, person_ids: tuple[int]):
         if not person_ids:
             return None
-        self.cur.execute("SELECT ADRMAIL FROM ADRESSEEMAIL WHERE IDINDIVIDU = %s", (person_ids,))
+        query = f"SELECT ADRMAIL FROM ADRESSEEMAIL WHERE IDINDIVIDU IN {self.tuple_to_str(person_ids)}"
+        self.cur.execute(query)
         data = self.cur.fetchall()
         cols = [desc[0] for desc in self.cur.description]
         return pd.DataFrame(data, columns=cols)
@@ -47,7 +52,8 @@ class AnnuaireHandler:
     def get_pn_phone(self, person_ids: tuple[int]):
         if not person_ids:
             return None
-        self.cur.execute("SELECT INDICATIFTEL, NUMTEL FROM TELEPHONE WHERE IDINDIVIDU = %s", (person_ids,))
+        query = f"SELECT INDICATIFTEL, NUMTEL FROM TELEPHONE WHERE IDINDIVIDU IN {self.tuple_to_str(person_ids)}"
+        self.cur.execute(query)
         data = self.cur.fetchall()
         cols = [desc[0] for desc in self.cur.description]
         return pd.DataFrame(data, columns=cols)
@@ -55,14 +61,15 @@ class AnnuaireHandler:
     def get_pn_adresse(self, person_ids: tuple[int]):
         if not person_ids:
             return None
-        self.cur.execute("""
-            SELECT 
+        query = f"""
+        SELECT 
                 CIVILITE, IDCIVILITE, ACHEMINEMENT, CODEPOSTAL, COMPLEMENTADR,
                 COMPLEMENTNOM, LIBVOIE, LIEUDITPOSTALOUBP, NOM, NUMVOIE,
                 PRENOM, CODEACTIONRECRUT, IDINDIVIDU
             FROM INDIVIDUADRESSE 
-            WHERE IDINDIVIDU = %s
-        """, (person_ids,))
+            WHERE IDINDIVIDU IN {self.tuple_to_str(person_ids)}
+        """
+        self.cur.execute(query)
         data = self.cur.fetchall()
         cols = [desc[0] for desc in self.cur.description]
         return pd.DataFrame(data, columns=cols)
@@ -70,7 +77,7 @@ class AnnuaireHandler:
     def get_person_functions_pn(self, person_ids: tuple[int]):
         if not person_ids:
             return None
-        query = """
+        query = f"""
         SELECT 
             rl.LIBELLE_LIEN,
             rt.LIB_TYPELIEN,
@@ -80,13 +87,12 @@ class AnnuaireHandler:
         FROM LIEN l
         JOIN REF_LIEN rl ON rl.IDREF_LIEN = l.IDREF_LIEN
         JOIN REF_TYPELIEN rt ON rt.IDREF_TYPELIEN = rl.IDREF_TYPELIEN
-        WHERE l.IDINDIVIDU = %s
+        WHERE l.IDINDIVIDU IN {self.tuple_to_str(person_ids)}
         """
-        self.cur.execute(query, (person_ids,))
+        self.cur.execute(query)
         data = self.cur.fetchall()
         cols = [desc[0] for desc in self.cur.description]
         return pd.DataFrame(data, columns=cols)
-
 
 
     def get_emails(self):
